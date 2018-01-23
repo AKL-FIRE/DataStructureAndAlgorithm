fa68439122b31311f76f3dcb8c7145a1b5896ef8
lcy <lcy19961018@163.com>
2018-01-23 16:39:15 +0800

    Initial Commit

---
+++ DataStructureAndAlgorithm/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DataStructureAndAlgorithm
+//
+//  Created by Apple  on 23/01/2018.
+//  Copyright © 2018 lcy. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
