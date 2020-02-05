1bf14501c703f52f0f58ffdbda78b8af05ae3752
lcy <lcy19961018@163.com>
2020-02-04 23:11:18 -0800

    include iterator

--- DataStructureAndAlgorithm/LinearList/linearList.hpp
+++ DataStructureAndAlgorithm/LinearList/linearList.hpp
@@ -10,13 +10,14 @@
 #define linearList_hpp
 
 #include <iostream>
+#include <iterator>
 #include "../Tool/utlity.h"
 
 template <typename T>
 class linearList
 {
 public:
-    virtual ~linearList() {};
+    virtual ~linearList() = default;
     virtual bool empty() const = 0;
     virtual int size() const = 0;
     virtual T& get(int theIndex) const = 0;
